401470398dfd238dc52ca613e29d855f84231f41
sneer <sneer4383@naver.com>
2020-07-01 16:43:05 +0900

    start

---
+++ c_ch2_1_ex1/c_ch2_1_1/c_ch2_1_ex2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("최진경\n최 진 경\n최  진  경");
+    return 0;
+}
---
+++ c_ch2_1_ex2/c_ch2_1_2/c_ch2_1_ex2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("최진경\n");
+    printf("관악학생생활관\n");
+    printf("010-xxxx-xxxx");
+    return 0;
+}
